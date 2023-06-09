a1a614a7a1175c0a827aee80f0bd128596e9fbe9
Stepan72 <st.nim@list.ru>
2023-06-09 18:40:46 +0600

    about completed v.04

--- container/AboutUs/AboutUs.jsx
+++ container/AboutUs/AboutUs.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+"use client";
+import React from "react";
+import "./AboutUs.scss";
 
-import './AboutUs.css';
-
-const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+const AboutUs = () => {
+  return (
+    <div
+      className="app__aboutus app__bg flex__center section__padding"
+      id="about"
+    >
+      <div className="app__aboutus-overlay flex__center">
+        <img src="/assets/G.png" alt="G-letter" />
+      </div>
+      <div className="app__aboutus-content flex__center">
+        <div className="app__aboutus-content-about">
+          <h1 className="headtext__cormorant">About Us</h1>
+          <img
+            src="/assets/spoon.png"
+            alt="about_spoon"
+            className="spoon__img"
+          />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
+        <div className="app__aboutus-content-knife flex__center">
+          <img src="/assets/knife.png" alt="about_knife" />
+        </div>
+        <div className="app__aboutus-content-history">
+          <h1 className="headtext__cormorant">Our History</h1>
+          <img
+            src="/assets/spoon.png"
+            alt="about_spoon"
+            className="spoon__img"
+          />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
--- container/Header/Header.jsx
+++ container/Header/Header.jsx
@@ -6,7 +6,10 @@ import { SubHeading } from "@/components";
 
 const Header = () => {
   return (
-    <div className="app__header app__wrapper section__padding" id="home">
+    <div
+      className="app__header app__wrapper app__bg section__padding"
+      id="home"
+    >
       <div className="app__wrapper_info">
         <SubHeading title="Chase the new flavour" />
         <h1 className="app__header-h1">The Key to Fine Dining</h1>
