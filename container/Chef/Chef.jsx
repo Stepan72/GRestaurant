cc11ec3b3f02cdc87500839c855221d84947bad8
Stepan72 <st.nim@list.ru>
2023-06-10 12:21:38 +0600

    chefs completed v.06

--- container/Chef/Chef.jsx
+++ container/Chef/Chef.jsx
@@ -1,8 +1,39 @@
+"use client";
 import React from "react";
+import { SubHeading } from "@/components";
+import "./Chef.scss";
 
-import "./Chef.css";
-
-const Chef = () => <div>Chef</div>;
+const Chef = () => {
+  return (
+    <div className="app__bg app__wrapper section__padding">
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+        <img src="/assets/chef.png" alt="chef-img" />
+      </div>
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef's Word" />
+        <h1 className="headtext__cormorant">What We Believe In</h1>
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <img src="/assets/quote.png" alt="quote-img" />
+            <p className="p__opensans">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <p className="p__opensans">
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src="/assets/sign.png" alt="sign-img" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chef;
 
