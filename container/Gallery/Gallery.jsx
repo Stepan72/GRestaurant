65e9b0fdd6750e391f4879d45117d3db4a1df6f7
Stepan72 <st.nim@list.ru>
2023-06-10 23:15:38 +0600

    gallery completed v.09

--- container/Gallery/Gallery.jsx
+++ container/Gallery/Gallery.jsx
@@ -32,7 +32,8 @@ const Gallery = () => {
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
         <p className="p__opensans mt-8" style={{ color: "#AAA" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do.
         </p>
         <button type="button" className="custom__button">
           View More
@@ -42,7 +43,7 @@ const Gallery = () => {
         <div className="app__gallery-images_container" ref={scrollRef}>
           {images.map((el, index) => {
             return (
-              <div className="app__gallery-images_container-card flex__center">
+              <div className="app__gallery-images_card flex__center">
                 <img src={el} key={index} alt="gallery-img" />
                 <BsInstagram className="gallery__image-icon" />
               </div>
